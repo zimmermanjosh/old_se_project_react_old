8978b28ad355b1951f84be1df13a10fd7f7d58e2
Joshua Zimmerman <Joshua.Zimmerman@lexialearning.com>
2024-01-25 19:05:56 -0600

    updated: pulled location data from brwoser for location

--- se_project_react/src/Header/Header.js
+++ se_project_react/src/Header/Header.js
@@ -1,8 +1,39 @@
 import "./Header.css";
 import DateTime from "./DateTime.js";
+import React, { useState, useEffect } from "react";
 
 const Header = ({ onCreateModal }) => {
   console.log("Header");
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    // Check if Geolocation is available in the browser
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        // Retrieve the latitude and longitude from the position object
+        const { latitude, longitude } = position.coords;
+
+        // You can use a reverse geocoding service to get the city and state
+        // Replace 'YOUR_API_KEY' with your actual OpenCage Geocoder API key
+        fetch(
+          `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=20740fa69bd84624bf45f4a801ef40c3`,
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            const city = data.results[0].components.city;
+            const state = data.results[0].components.state;
+
+            // Update the location state with the city and state
+            setLocation(`${city}, ${state}`);
+          })
+          .catch((error) => {
+            console.error("Error fetching location data:", error);
+          });
+      });
+    } else {
+      console.error("Geolocation is not available in this browser.");
+    }
+  }, []);
 
   return (
     <header className="header">
@@ -16,6 +47,7 @@ const Header = ({ onCreateModal }) => {
         <div>
           <DateTime />
         </div>
+        <div className="header__location">{location || "Loading..."}</div>
       </div>
       <div className="header__avatar-logo">
         <div>
@@ -23,7 +55,7 @@ const Header = ({ onCreateModal }) => {
             add new clothes
           </button>
         </div>
-        <div>name</div>
+        <div className="header__name">what's my name</div>
         <div>
           <img
             src={require("../images/dashboard/avatar.svg").default}
